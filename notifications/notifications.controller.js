89d180e29763f282ad9161cb9109181f06234e53
Alex Algarate <alex.algarate@gmail.com>
2025-11-19 18:58:06 +0100

    feat: add notifications

---
+++ notifications/notifications.controller.js
@@ -0,0 +1,22 @@
+import { buildNotification } from './notifications.view.js';
+
+export function notificationsController(notificationsContainer) {
+  const showNotification = (message, type = 'success') => {
+    const newNotification = document.createElement('div');
+    newNotification.innerHTML = buildNotification(message, type);
+
+    newNotification.querySelector('button').addEventListener('click', () => {
+      newNotification.remove();
+    });
+
+    notificationsContainer.appendChild(newNotification);
+
+    setTimeout(() => {
+      newNotification.remove();
+    }, 5000);
+  };
+
+  return {
+    showNotification,
+  };
+}
---
+++ notifications/notifications.view.js
@@ -0,0 +1,8 @@
+export const buildNotification = (message, type) => {
+  return `
+    <div style="display: flex; gap: 1rem; width: fit-content" class="${type}">
+        <h3>${message}</h3>
+        <button>❌</button>
+    </div>
+    `;
+};
