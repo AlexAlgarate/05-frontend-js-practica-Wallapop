7fe7726dbd612669733a8d9085c3976f60b7340a
Alex Algarate <alex.algarate@gmail.com>
2025-11-22 19:11:52 +0100

    feat: refactor create ad controller

--- createAd/createAd.controller.js
+++ createAd/createAd.controller.js
@@ -1,49 +1,58 @@
 import { createAd } from './createAd.model.js';
+import {
+  constants,
+  querySelectors,
+  switchOptionsCreateAd,
+} from '../utils/constants.js';
 
-export const createAdController = (createAdContainer) => {
-  initSubmit(createAdContainer);
-  initSwitch(createAdContainer);
-};
-
-const initSubmit = (createAdContainer) => {
-  createAdContainer.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const adContent = getFormData(createAdContainer);
-
-    try {
-      await createAd(adContent);
-      alert('Anuncio creado correctamente');
-      setTimeout(() => {
-        window.location.href = '/';
-      }, 3000);
-    } catch (error) {
-      alert(error);
-    }
-  });
-};
-const getFormData = (createAdContainer) => ({
-  name: createAdContainer.querySelector('#productName').value.trim(),
-  description: createAdContainer.querySelector('#productDescription').value.trim(),
-  price: Number(createAdContainer.querySelector('#productPrice').value),
-  imageURL: createAdContainer.querySelector('#productImage').value.trim(),
-  operationType: createAdContainer.querySelector('#switchCompraVenta').value,
+const getFormData = (form) => ({
+  name: form.querySelector(querySelectors.createAdForm.productName).value.trim(),
+  description: form
+    .querySelector(querySelectors.createAdForm.productDescription)
+    .value.trim(),
+  price: Number(form.querySelector(querySelectors.createAdForm.productPrice).value),
+  imageURL: form.querySelector(querySelectors.createAdForm.productImage).value.trim(),
+  operationType: form.querySelector(querySelectors.createAdForm.switchSalePurchase)
+    .value,
 });
 
-const initSwitch = (createAdContainer) => {
-  const checkboxSwitch = createAdContainer.querySelector('#switchCompraVenta');
-  const label = document.querySelector('#labelCompraVenta');
+const handleSubmit = async (event, form) => {
+  event.preventDefault();
 
-  const venta = 'Venta';
-  const compra = 'Compra';
+  const adContent = getFormData(form);
+
+  try {
+    await createAd(adContent);
+    alert('Anuncio creado correctamente');
+    setTimeout(() => {
+      window.location.href = '/';
+    }, constants.redirectDelay);
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const initSwitch = (form) => {
+  const checkboxSwitch = form.querySelector(
+    querySelectors.createAdForm.switchSalePurchase
+  );
+  const label = form.querySelector(querySelectors.createAdForm.labelSalePurhase);
 
   const updateSwitchLabel = () => {
-    const value = checkboxSwitch.checked ? venta : compra;
+    const value = checkboxSwitch.checked
+      ? switchOptionsCreateAd.sale
+      : switchOptionsCreateAd.purchase;
     label.textContent = value;
     checkboxSwitch.value = value;
   };
 
   updateSwitchLabel();
-
   checkboxSwitch.addEventListener('change', updateSwitchLabel);
+};
+
+export const createAdController = (createAdContainer) => {
+  initSwitch(createAdContainer);
+  createAdContainer.addEventListener('submit', (event) =>
+    handleSubmit(event, createAdContainer)
+  );
 };
