c2788106c0b11e695f23c5f716d5b3dc6807ad21
Alex Algarate <alex.algarate@gmail.com>
2025-11-22 21:49:46 +0100

    feat: clean ad  detail page removing unnecessary h1

--- adDetail.js
+++ adDetail.js
@@ -13,7 +13,5 @@ const adId = searchParams.get('adId');
 if (!adId) {
   window.location.href = '/';
 }
-const h1 = document.querySelector('h1');
-h1.textContent = h1.textContent + ` ${adId}`;
 
 adDetailController(adDetail, adId);
