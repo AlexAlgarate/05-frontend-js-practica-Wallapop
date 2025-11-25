bd7d7d58e3aadbcca6e0122e1e08912eaec9f854
Alex Algarate <alex.algarate@gmail.com>
2025-11-25 21:05:51 +0100

    feat: add navbar to create-ad page

--- createAd.js
+++ createAd.js
@@ -1,5 +1,10 @@
 import { createAdController } from './createAd/createAd.controller.js';
+import { sessionController } from './session/session.controller.js';
 
 const createAdForm = document.querySelector('form');
+const sessionContainer = document.querySelector('header');
+
+
+sessionController(sessionContainer);
 
 createAdController(createAdForm);
